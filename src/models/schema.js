21e76cff0105e316e76451b1b45840a9fea97884
sarkarji <98710029+sourajsarkar@users.noreply.github.com>
2023-06-17 11:08:12 +0530

    updated

--- src/models/schema.js
+++ src/models/schema.js
@@ -1,24 +1,20 @@
-// const mongoose= require('mongoose');
+const mongoose = require("mongoose");
 
-// const employeeSchema =new mongoose.Schema({
-//     name: {
-//         type:String,
-//         required:true
+const employeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
+const Schema = new mongoose.model("Schema", employeeSchema);
 
-//     },
-//     email: {
-//         type:String,
-//         required:true,
-//         unique:true
-
-//     },
-//     message : {
-//         type:String,
-//         required:true
-
-//     }
-
-// });
-// const Schema=new mongoose.model("Schema",employeeSchema);
-
-// module.exports=Schema;
+module.exports = Schema;
